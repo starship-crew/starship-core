9710b4a6a8a9d8532837961a51a3282d96bba1fa
Mark Meliksetyan <markmelix@gmail.com>
2023-04-11 18:25:40 +0300

    implement interactive user creation via admin panel

--- starship/admin.py
+++ starship/admin.py
@@ -4,7 +4,7 @@ from starship import login_manager
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
 from flask_login import current_user, login_required, login_user, logout_user
-from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from wtforms import BooleanField, IntegerField, PasswordField, StringField, SubmitField
 from flask import Blueprint, abort, flash, redirect, url_for, render_template
 from starship import db
 from starship.helper import redirect_url
@@ -92,3 +92,47 @@ def toggle_admin_state(user_id):
         user.is_admin = not user.is_admin
         db.session.commit()
     return redirect(redirect_url("admin_bp.dashboard"))
+
+
+class UserCreationForm(FlaskForm):
+    login = StringField("Login", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    currency = IntegerField("Initial currency", default=0)
+    is_admin = BooleanField("Admin", default=False)
+    submit = SubmitField("Create")
+
+
+@admin_bp.route("/create_user", methods=["GET", "POST"])
+@admin_required
+def create_user():
+    form = UserCreationForm()
+
+    if form.validate_on_submit():
+        exists = db.session.query(User).filter_by(login=form.login.data).first()
+        if not exists:
+            user = User()
+            user.login = form.login.data
+            user.set_password(form.password.data)
+            user.currency = form.currency.data
+            user.is_admin = form.is_admin.data
+
+            db.session.add(user)
+            db.session.commit()
+
+            return redirect(url_for("admin_bp.dashboard"))
+
+        flash("User with this login already exists")
+
+    return render_template("admin/create_user.html", form=form, title="User creation")
+
+
+@admin_bp.route("/create_crew")
+@admin_required
+def create_crew():
+    return {}
+
+
+@admin_bp.route("/create_ship")
+@admin_required
+def create_ship():
+    return {}
