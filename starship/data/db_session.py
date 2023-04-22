a786f08711beada29293d8d1d930888b48243c7a
Mark Meliksetyan <markmelix@gmail.com>
2023-04-22 11:46:54 +0300

    remove uneeded imports from db_session.py

--- starship/data/db_session.py
+++ starship/data/db_session.py
@@ -1,11 +1,9 @@
 import sqlalchemy as sa
 import os
 
-from flask import request
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
 import sqlalchemy.ext.declarative as dec
-from urllib.parse import urlparse, urljoin
 
 SqlAlchemyBase = dec.declarative_base()
 
